719b2a9ce7fddb91bb65f5d5d6ecb84a4706e5ff
Nitish Kumar Singh <nitishk72@gmail.com>
2021-01-20 12:01:15 +0530

    video completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +7,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ImagePicker _picker = ImagePicker();
+  PickedFile _file;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Image Picker'),
       ),
+      body: ListView(
+        children: [
+          _file == null
+              ? Text(
+                  'No Image Selecte',
+                )
+              : Image.asset(
+                  _file.path,
+                ),
+          RaisedButton(
+            onPressed: _openImagePicker,
+            child: Text('Open Image Picker'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _openImagePicker() async {
+    final _source = ImageSource.camera;
+    PickedFile file = await _picker.getImage(source: _source);
+    if (file != null) {
+      _file = file;
+      setState(() {});
+    }
   }
 }
